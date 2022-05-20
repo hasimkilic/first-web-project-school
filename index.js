3c521991399c3dd6ce088cbbed347abdbf5acf36
Haşim Kılıç <93584838+hasimkilic@users.noreply.github.com>
2022-05-20 10:36:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let btn = $('#yukari_buton');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+window.onscroll = function() {
+    scrollFunction()
+};
